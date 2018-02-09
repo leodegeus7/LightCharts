98c84cbf17bdae95cf2eedb118ada39882a5bbd2
Leonardo Geus <leoardodegeus@gmail.com>
2018-02-09 10:41:27 -0200

    First Commit

---
+++ LightCharts/LightCharts.h
@@ -0,0 +1,19 @@
+//
+//  LightCharts.h
+//  LightCharts
+//
+//  Created by Leonardo Geus on 09/02/2018.
+//  Copyright © 2018 Leonardo Geus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightCharts.
+FOUNDATION_EXPORT double LightChartsVersionNumber;
+
+//! Project version string for LightCharts.
+FOUNDATION_EXPORT const unsigned char LightChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightCharts/PublicHeader.h>
+
+
